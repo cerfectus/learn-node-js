2d65ac23ea5233396973c04a2af7726f4d3ef7ed
lucas <cerfectus@gmail.com>
2019-08-28 15:21:00 -0500

    post request

--- express.js
+++ express.js
@@ -1,13 +1,21 @@
 var express = require("express");
+var bodyParser = require("body-parser")
+
 var app = express()
+var urlencodedparser = bodyParser.urlencoded({extended: false})
 
 app.set("view engine", "ejs")
+app.use("/assets", express.static("assets"))
 
 app.get("/", (req,res) => {
     res.render("index")
 })
 app.get("/contact", (req, res)=> {
-    res.render("contact")
+    res.render("contact", {qs: req.query})
+})
+app.post("/contact", urlencodedparser, (req, res)=> {
+    console.log(req.body)
+    res.render("contact-success", {data: req.body})
 })
 app.get("/profile/:id", (req,res)=> {
     var data = {age: 29, job : "Ninja", hobbies: ["run","swim","eat","prey","love"]}
@@ -15,5 +23,5 @@ app.get("/profile/:id", (req,res)=> {
 })
 //req.params.id
 app.listen(3000, (req,res)=> {
-    console.log("hola perros")
+    console.log("listening")
 })
